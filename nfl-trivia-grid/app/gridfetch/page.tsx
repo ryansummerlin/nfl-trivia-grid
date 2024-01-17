2dd49ff181b1113a37b91fb76079a6d9918f5f49
ryansummerlin <ryansummerlin777@gmail.com>
2024-01-16 21:25:11 -0800

    deleted playerData page

--- nfl-trivia-grid/app/gridfetch/page.tsx
+++ nfl-trivia-grid/app/gridfetch/page.tsx
@@ -7,16 +7,15 @@ export default function Page() {
         })
     }
 
-    const getTeamName = async() => {
-        await fetch('api/teamName', {
-            method: 'GET'
-        })
-    }
+    // const getTeamName = async() => {
+    //     await fetch('api/teamName', {
+    //         method: 'GET'
+    //     })
+    // }
 
     return (
         <main>
             <button onClick={makeAPICall}>Make api call</button>
-            <button onClick={getTeamName}>get team name</button>
         </main>
 
         )
